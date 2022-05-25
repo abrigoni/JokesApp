ad7b5b846f2dc42e94b18636cb8fb06297bb2d91
abrigoni <brian.rigoni1@gmail.com>
2022-05-25 15:44:23 -0300

    stable build without rnvi

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { Pressable } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { IconProps } from 'react-native-vector-icons/Icon';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import { IconProps } from 'react-native-vector-icons/Icon';
 
 interface Props {
   iconProps: IconProps;
@@ -11,7 +11,7 @@ interface Props {
 const IconButton: FC<Props> = ({onPress, iconProps}) => {
   return (
     <Pressable onPress={onPress}>
-      <Icon {...iconProps} />
+      {/* <Icon {...iconProps} /> */}
     </Pressable>
   );
 };
