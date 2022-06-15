23f135d68b4a6f0618aa516cd2dfeac586ac474b
abrigoni <brian.rigoni1@gmail.com>
2022-06-15 14:16:27 -0300

    firebase cloud messaging first attempts with working environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import AppContextProvider from './context/AppContext';
 import {ApolloProvider} from '@apollo/client';
 import {client} from './graphql/client';
 import AppNavigator from './navigation/AppNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
 
 const App = () => {
+  useEffect(() => {
+    requestUserPermission();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
   return (
     <ApolloProvider client={client}>
       <AppContextProvider>
