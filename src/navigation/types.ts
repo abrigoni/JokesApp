55927d344cc4fb9c625901d5331b078e7e67fdc7
abrigoni <brian.rigoni1@gmail.com>
2022-06-20 11:02:07 -0300

    rn firebase auth config + route param

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,9 +1,11 @@
 import { HOME_ROUTE } from "../screens/Home";
 import { SAVED_JOKES_ROUTE } from "../screens/SavedJokes";
+import { SIGN_IN_ROUTE } from "../screens/SignInScreen";
 
 export type AppNavigatorStackParamList = {
   [HOME_ROUTE]: undefined;
   [SAVED_JOKES_ROUTE]: undefined;
+  [SIGN_IN_ROUTE]: undefined;
 };
 
 declare global {
